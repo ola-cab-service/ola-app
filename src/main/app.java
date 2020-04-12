a70bb026c2c22c4b938eb0bae1bfea874ac01eb9
sushant <sushantsangam92@gmail.com>
2020-04-12 08:37:16 +0000

    commit my changes

--- src/main/app.java
+++ src/main/app.java
@@ -2,8 +2,11 @@ class App{
 
 	public static void main(String[] args)
 	{
+<<<<<<< HEAD
 		s.o.p("hello world again");	
 
+=======
+>>>>>>> parent of a6d31bb... Update app.java
 		s.o.p("hello world");	
 	}
 	public static void myMethod()
