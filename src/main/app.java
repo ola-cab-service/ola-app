21c697e4071829f54a97cffc69b87c6a92c4b096
sushant <sushantsangam92@gmail.com>
2020-04-12 08:18:48 +0000

    updated my first phase of commi

--- src/main/app.java
+++ src/main/app.java
@@ -3,10 +3,12 @@ class App{
 	public static void main(String[] args)
 	{
 		s.o.p("hello world again");	
+
+		s.o.p("hello world");	
 	}
 	public static void myMethod()
 	{
 		s.o.p("my method");	
+	        s.o.p("my method 2");
 	}
 
-}
