16736b1818e50a1fd3a812e9f2d15f37e720e11e
sushant <sushantsangam92@gmail.com>
2020-04-12 13:41:17 +0000

    ola101:updated as part of ola101 changes

--- src/main/app.java
+++ src/main/app.java
@@ -2,13 +2,20 @@ class App{
 
 	public static void main(String[] args)
 	{
-		s.o.p("hello world again");	
+		s.o.p("hello world again");
 		s.o.p("hello world");	
 	
 	public static void myMethod()
 	{
 		s.o.p("my method");	
-	        s.o.p("my method 2 canges);
+	        s.o.p("my method 2 time");
+		s.o.p("my method");
 
 	}
+
+	public void myMethod2(){
+		s.o.p("my branch");
+	}
+	}
+}
 
