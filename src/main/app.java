248d25e58dd03ab86593da2b460924d615017a04
sushant <sushantsangam92@gmail.com>
2020-04-12 08:43:34 +0000

    app changes for ola

--- src/main/app.java
+++ src/main/app.java
@@ -2,16 +2,12 @@ class App{
 
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 		s.o.p("hello world again");	
-
-=======
->>>>>>> parent of a6d31bb... Update app.java
 		s.o.p("hello world");	
 	}
 	public static void myMethod()
 	{
 		s.o.p("my method");	
-	        s.o.p("my method 2");
+	        s.o.p("my method 2 canges);
 	}
 
