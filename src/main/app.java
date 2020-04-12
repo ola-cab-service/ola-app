7824c317c9a4f224b5cdaad351433dab777183b7
sushant321 <41298843+sushant321@users.noreply.github.com>
2020-04-12 11:05:02 +0530

    Update app.java

--- src/main/app.java
+++ src/main/app.java
@@ -1,9 +1,12 @@
-ass App{
+class App{
 
 	public static void main(String[] args)
 	{
-		s.o.p("hello world");
-
+		s.o.p("hello world");	
+	}
+	public static void myMethod()
+	{
+		s.o.p("my method");	
 	}
 
 }
